6bec837bc22f411838c3f05be58f3cd132e0872d
Chocho Iwasaki <i.chocho1220@gmail.com>
2026-05-26 22:04:05 +0900

    Initial Commit

---
+++ MeshiTero/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeshiTero
+//
+//  Created by 岩﨑蝶々 on 2026/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeshiTero/MeshiTeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshiTeroApp.swift
+//  MeshiTero
+//
+//  Created by 岩﨑蝶々 on 2026/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MeshiTeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
